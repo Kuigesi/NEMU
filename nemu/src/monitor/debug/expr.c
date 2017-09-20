cc6a0de28b676203f20929c9d6dd07f5bf154354
ruiqigao <ruiqi.gao@intel.com>
2017-09-20 11:47:42 +0000

    >  compile 161250026 1210077937 Linux b350b48e2dae 4.9.41-moby #1 SMP Wed Sep 6 00:05:16 UTC 2017 x86_64 GNU/Linux  11:47:42 up  5:06,  1 user,  load average: 0.00, 0.00, 0.00 c858443c605fc1b26b804658133b8ef7a3dfb0e8

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -1,4 +1,5 @@
 #include "nemu.h"
+#include <stdlib.h>
 
 /* We use the POSIX regex functions to process regular expressions.
  * Type 'man regex' for more information about POSIX regex functions.
@@ -130,7 +131,131 @@ static bool make_token(char *e) {
 
   return true;
 }
+static bool check_parentheses(int p,int q)
+{
+	if(tokens[p].type!='('||tokens[q].type!=')')
+	{
+		return false;
+	}
+	else
+	{
+		int parnum = 1;
+		int m;
+		for( m = p+1;m<q;m++)
+		{
+	            if(tokens[m].type=='(' )
+		    {
+                          parnum++;
+		    }
+                    if(tokens[m].type==')' )
+		    {
+			  parnum--;
+		    }
+                    if(parnum==0)
+		    {
+			    return false;
+		    }
 
+	        }
+               return true;
+	}
+}
+static int op_location(int p,int q)
+{
+   int parnum = 0;
+   int m;
+   bool detected1,detected2,possess1,possess2;
+   possess1=possess2=false;
+   int location1,location2;
+   location1=location2= -1;
+   for( m = p;m<q+1;m++)
+   {
+	   detected1=detected2=false;
+	   if(tokens[m].type=='('  )
+	   {
+                  parnum++;
+	   }
+           if(tokens[m].type==')' )
+	   {
+		   parnum--;
+	   }
+	   if(parnum==0)
+	   {
+		   if(tokens[m].type=='+'||tokens[m].type=='-' )
+		   {
+			   detected1 = true;
+		   }
+		   if(tokens[m].type=='*'||tokens[m].type=='/' )
+		   {
+			   detected2 = true;
+		   }
+	   }
+	   if(detected1 )
+	   {
+                 possess1 = true;
+		 location1 = m;
+	   }
+	   if(detected2 )
+	   {
+		 possess2 = true;
+		 location2 = m;
+	   }
+   }
+   if(possess1 )
+   {
+           return location1;
+   }
+   else
+   {
+	   if(possess2 )
+	   {
+                 return location2;
+	   }
+	   else
+	   {
+		   return -1;
+	   }
+   }
+
+}
+uint32_t eval(int p,int q)
+{
+  if(p==q)
+  {
+    int val = atoi(tokens[p].str);
+    return val;
+  }
+  else
+  {
+    if(check_parentheses(p, q))
+    {
+       return eval(p+1,q-1);
+    }
+    else
+    {
+       int op = op_location(p,q);
+       if (op==-1)
+       {
+	       return 0;
+       }
+       int val1 = eval(p,op-1);
+       int val2 = eval(op+1,q);
+       switch(tokens[op].type )
+       {
+	   case '+' : return val1+val2;
+		      break;
+           case '-' : return val1-val2;
+		      break;
+           case '*' : return val1*val2;
+		      break;
+           case '/' : return val1/val2;
+		      break;
+           default  : return 0;
+		      break;
+       }
+    }
+  }
+}
 uint32_t expr(char *e, bool *success) {
   if (!make_token(e)) {
     *success = false;
@@ -138,6 +263,7 @@ uint32_t expr(char *e, bool *success) {
   }
 
   /* TODO: Insert codes to evaluate the expression. */
+  
+  return eval(0,nr_token-1);
 
-  return 0;
 }
