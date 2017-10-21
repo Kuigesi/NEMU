99aef0f93b5a15c6bd069fe8e32f041f06c984a1
ruiqigao <ruiqi.gao@intel.com>
2017-10-21 09:21:09 +0000

    >  compile 161250026 1210077937 Linux b350b48e2dae 4.9.41-moby #1 SMP Wed Sep 6 00:05:16 UTC 2017 x86_64 GNU/Linux  09:21:09 up  6:41,  4 users,  load average: 0.00, 0.00, 0.00 ebef258819b22da9c6ab2b8a6d637dfd6ea29742

--- nemu/src/cpu/exec/cc.c
+++ nemu/src/cpu/exec/cc.c
@@ -14,14 +14,41 @@ void rtl_setcc(rtlreg_t* dest, uint8_t subcode) {
   // TODO: Query EFLAGS to determine whether the condition code is satisfied.
   // dest <- ( cc is satisfied ? 1 : 0)
   switch (subcode & 0xe) {
-    case CC_O:
-    case CC_B:
-    case CC_E:
-    case CC_BE:
-    case CC_S:
-    case CC_L:
-    case CC_LE:
-      TODO();
+    case CC_O:if(cpu.OF==1)
+	      {
+		   (*dest) = 1;
+	      }
+	      break;
+    case CC_B:if(cpu.CF==1)
+	      {
+		      (*dest) = 1;
+	      }
+              break;
+    case CC_E:if(cpu.ZF==1)
+	      {
+		      (*dest) = 1;
+	      }
+	      break;
+    case CC_BE:if(cpu.ZF==1||cpu.CF==1)
+	       {
+		       (*dest) = 1;
+	       }
+	       break;
+    case CC_S:if(cpu.SF==1)
+	      {
+		      (*dest) = 1;
+	      }
+	      break;
+    case CC_L:if(cpu.SF!=cpu.OF)
+	      {
+		      (*dest) = 1;
+	      }
+	      break;
+    case CC_LE:if(cpu.SF!=cpu.OF||cpu.ZF==1)
+	       {
+		       (*dest) = 1;
+	       }
+	       break;
     default: panic("should not reach here");
     case CC_P: panic("n86 does not have PF");
   }
