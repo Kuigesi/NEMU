050036435afe9edd78d823c843a94f86c74c173a
ruiqigao <ruiqi.gao@intel.com>
2017-11-03 16:34:08 +0000

    >  compile 161250026 1210077937 Linux b350b48e2dae 4.9.41-moby #1 SMP Wed Sep 6 00:05:16 UTC 2017 x86_64 GNU/Linux  16:34:08 up  4:51,  4 users,  load average: 0.04, 0.05, 0.07 50fabca2d8f3931b8273c7a41ec1a2fd91d6f0e

--- nemu/src/cpu/exec/arith.c
+++ nemu/src/cpu/exec/arith.c
@@ -3,11 +3,6 @@
 
 
 
-make_EHelper(neg) {
-  TODO();
-
-  print_asm_template1(neg);
-}
 
 make_EHelper(adc) {
   rtl_add(&t2, &id_dest->val, &id_src->val);
@@ -115,7 +110,25 @@ make_EHelper(dec) {
  rtl_set_OF(&t0);
 
  print_asm_template1(dec);
-}	
+}
+make_EHelper(neg) {
+id_src->val = 0;
+rtl_sub(&t2, &id_src->val, &id_dest->val);
+operand_write(id_dest, &t2);
+
+rtl_update_ZFSF(&t2, id_dest->width);
+
+rtl_sltu(&t0, &id_src->val, &t2);
+rtl_set_CF(&t0);
+
+rtl_xor(&t0, &id_src->val, &id_dest->val);
+rtl_xor(&t1, &id_src->val, &t2);
+rtl_and(&t0, &t0, &t1);
+rtl_msb(&t0, &t0, id_dest->width);
+rtl_set_OF(&t0);
+
+print_asm_template1(neg);
+}
 make_EHelper(cmp){
  rtl_sub(&t2, &id_dest->val, &id_src->val);
 
