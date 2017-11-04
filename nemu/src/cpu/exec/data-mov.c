ae62a3b0432b0e70fa40d552e2e66aef45a3210a
ruiqigao <ruiqi.gao@intel.com>
2017-11-04 03:13:08 +0000

    >  compile 161250026 1210077937 Linux b350b48e2dae 4.9.41-moby #1 SMP Wed Sep 6 00:05:16 UTC 2017 x86_64 GNU/Linux  03:13:08 up 15:30,  7 users,  load average: 0.01, 0.03, 0.08 f5bcdf6bf51bf4f0dff66e1a50b52f53f01e2d

--- nemu/src/cpu/exec/data-mov.c
+++ nemu/src/cpu/exec/data-mov.c
@@ -102,10 +102,14 @@ make_EHelper(cltd) {
 
 make_EHelper(cwtl) {
   if (decoding.is_operand_size_16) {
-    TODO();
+    rtl_lr(&t0,0,1);
+    rtl_sext(&t0,&t0,1);
+    rtl_sr(0,2,&t0);
   }
   else {
-    TODO();
+    rtl_lr(&t0,0,2);
+    rtl_sext(&t0,&t0,2);
+    rtl_sr(0,4,&t0);
   }
 
   print_asm(decoding.is_operand_size_16 ? "cbtw" : "cwtl");
