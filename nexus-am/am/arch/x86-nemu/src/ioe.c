f6b75d9d28041f40949392e688bc75ffe95d10e4
ruiqigao <ruiqi.gao@intel.com>
2017-11-04 06:15:37 +0000

    pa2.3 finished

--- nexus-am/am/arch/x86-nemu/src/ioe.c
+++ nexus-am/am/arch/x86-nemu/src/ioe.c
@@ -1,5 +1,6 @@
 #include <am.h>
 #include <x86.h>
+#include <stdio.h>
 
 #define RTC_PORT 0x48   // Note that this is not standard
 static unsigned long boot_time;
@@ -9,7 +10,10 @@ void _ioe_init() {
 }
 
 unsigned long _uptime() {
-  return 0;
+  unsigned long temp;
+  temp = inl(RTC_PORT);
+  temp = temp - boot_time;
+     return temp;
 }
 
 uint32_t* const fb = (uint32_t *)0x40000;
@@ -23,8 +27,13 @@ extern void* memcpy(void *, const void *, int);
 
 void _draw_rect(const uint32_t *pixels, int x, int y, int w, int h) {
   int i;
-  for (i = 0; i < _screen.width * _screen.height; i++) {
-    fb[i] = i;
+  int j;
+  for (i = x; i < x + w; i++)
+  {
+	  for (j = y; j < y + h; j++)
+	  {
+		  fb[j*400 + i] =pixels[(j-y)*w +i-x];
+	  }
   }
 }
 
@@ -32,5 +41,18 @@ void _draw_sync() {
 }
 
 int _read_key() {
+  uint8_t status_t;
+  uint32_t data_t;
+  int temp;
+ status_t = inb(0x64);
+ if(status_t==0x1)
+ {
+   data_t = inl(0x60);
+   temp = data_t;
+   return temp;
+ }
+ else
+ {
   return _KEY_NONE;
+ }
 }
