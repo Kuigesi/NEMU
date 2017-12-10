13d77459280818b30187340fffd8999d16b87e6e
ruiqigao <ruiqi.gao@intel.com>
2017-12-10 07:41:27 +0000

    >  compile 161250026 1210077937 Linux b350b48e2dae 4.9.41-moby #1 SMP Wed Sep 6 00:05:16 UTC 2017 x86_64 GNU/Linux  07:41:27 up  3:46,  5 users,  load average: 0.00, 0.00, 0.00 2825c5152abd6028900a27b8b462a814976e4b07

--- nemu/src/memory/memory.c
+++ nemu/src/memory/memory.c
@@ -1,5 +1,6 @@
 #include "nemu.h"
 #include "device/mmio.h"
+#include "memory/mmu.h"
 
 #define PMEM_SIZE (128 * 1024 * 1024)
 
@@ -44,4 +45,41 @@ uint32_t vaddr_read(vaddr_t addr, int len) {
 
 void vaddr_write(vaddr_t addr, int len, uint32_t data) {
   paddr_write(addr, len, data);
+}
+paddr_t page_translate (vaddr_t addr,int w)
+{
+    uintptr_t tp0,tp1,tpbase0,tpbase1,tpaddr0,tpaddr1,addr_t;
+    addr_t = (uintptr_t)addr;
+    tp0 = addr_t>>22;
+    tp1 = ((addr_t<<10)>>10)>>12;
+    tpbase0 = cpu.cr3.page_directory_base;
+    tpbase0 = tpbase0<<12;
+    tpaddr0 = tpbase0 + (tp0<<2);
+    PDE* pde_t;
+    pde_t = (PDE*)tpaddr0;
+    if(pde_t->present!=1)
+    {
+        assert(0);
+    }
+    pde_t->accessed = 1;
+    tpbase1 = pde_t->page_frame;
+    tpbase1 = tpbase1<<12;
+    tpaddr1 = tpbase1 + (tp1<<2);
+    PTE* pte_t;
+    pte_t = (PTE*)tpaddr1;
+    if(pte_t->present!=1)
+    {
+        assert(0);
+    }
+    pte_t->accessed = 1;
+    if(w==1)
+    {
+        pte_t->dirty = 1;
+    }
+    uint32_t pa_tt;
+    pa_tt =  pte_t->page_frame;
+    pa_tt = pa_tt <<12;
+    pa_tt = pa_tt + (addr_t&0x3ff);
+    return (paddr_t)pa_tt;
+    
 }
