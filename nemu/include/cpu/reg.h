44a2ab5e3dce3b828caa2d506db43bf4e5ed0170
ruiqigao <ruiqi.gao@intel.com>
2017-12-10 05:36:24 +0000

    >  compile 161250026 1210077937 Linux b350b48e2dae 4.9.41-moby #1 SMP Wed Sep 6 00:05:16 UTC 2017 x86_64 GNU/Linux  05:36:24 up  1:40,  5 users,  load average: 0.15, 0.03, 0.01 9c72fcbb46219d515922ad028b542762ac859cb0

--- nemu/include/cpu/reg.h
+++ nemu/include/cpu/reg.h
@@ -55,6 +55,31 @@ typedef union{
    uint32_t LIMIT :32; 
    } IDTR;
    rtlreg_t CS;
+   union
+   {
+     struct
+     {
+	     uint32_t protect_enable    :1;
+	     uint32_t dont_care         :30;
+	     uint32_t paging            :1;
+     };
+     uint32_t val;
+
+   } cr0;
+   union
+   {
+     struct
+     {
+	     uint32_t pad0              :3;
+	     uint32_t page_write_through :1;
+	     uint32_t page_cache_disable :1;
+	     uint32_t pad1              :7;
+	     uint32_t page_directory_base :20;
+
+     };
+     uint32_t val;
+   } cr3;
+
   };
 } CPU_state;
 
