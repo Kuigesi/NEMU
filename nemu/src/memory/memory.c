de76572a3eeed14c7c97bde69a49c7722d12d8d6
ruiqigao <ruiqi.gao@intel.com>
2017-12-16 11:58:29 +0000

    >  compile 161250026 1210077937 Linux b350b48e2dae 4.9.41-moby #1 SMP Wed Sep 6 00:05:16 UTC 2017 x86_64 GNU/Linux  11:58:29 up 38 min,  5 users,  load average: 0.01, 0.03, 0.00 f3388699c7e5edb6f768b8bd9dd6d69d2f42624

--- nemu/src/memory/memory.c
+++ nemu/src/memory/memory.c
@@ -43,7 +43,7 @@ uint32_t vaddr_read(vaddr_t addr, int len) {
 	uint32_t tp;
 	tp = (addr<<20)>>20;
 	tp = tp + (uint32_t)len -1;
-	if((tp>0xfff)&&(cpu.cr0.paging==1))
+	if(tp>0xfff)
 	{
 		assert(0);
 	}
@@ -58,9 +58,8 @@ void vaddr_write(vaddr_t addr, int len, uint32_t data) {
 	uint32_t tp;
 	tp = (addr<<20)>>20;
 	tp = tp + (uint32_t)len -1;
-	if((tp>0xfff)&&(cpu.cr0.paging==1))
+	if(tp>0xfff)
 	{
-		printf("addr = %x len =%x\n",addr,len);
 		assert(0);
 	}
 	else
@@ -75,47 +74,41 @@ paddr_t page_translate (vaddr_t addr,int w)
     {
 	    return addr;
     }
-    printf("good\n");
-    uintptr_t tp0,tp1,tpbase0,tpbase1,tpaddr0,tpaddr1,addr_t;
-    addr_t = (uintptr_t)addr;
-    printf("addr = %lx \n",(uintptr_t)addr_t);
+    uint32_t tp0,tp1,tpbase0,tpbase1,tpaddr0,tpaddr1,addr_t,temp;
+    addr_t = (uint32_t)addr;
     tp0 = addr_t>>22;
-    tp1 = ((addr_t<<42)>>42)>>12;
-    printf("good\n");
+    tp1 = ((addr_t<<10)>>10)>>12;
     tpbase0 = cpu.cr3.page_directory_base;
-    printf("cr3 = %lx \n",(uintptr_t)tpbase0);
-    printf("good\n");
     tpbase0 = tpbase0<<12;
     tpaddr0 = tpbase0 + (tp0<<2);
-    PDE* pde_t;
-    pde_t = (PDE*)tpaddr0;
-    printf("good\n");
-    printf("pde_t = %lx \n",(uintptr_t)pde_t);
-    if(pde_t[0].present!=1)
+    PDE pde_t;
+    pde_t = (PDE)paddr_read(tpaddr0,4);
+    if(pde_t.present!=1)
     {
-	printf("pde_t = %lx \n",(uintptr_t)pde_t);
         assert(0);
     }
-    printf("good\n");
-    printf("pde_t = %lx \n",(uintptr_t)pde_t);
-    pde_t[0].accessed = 1;
-    tpbase1 = pde_t->page_frame;
+    pde_t.accessed = 1;
+    temp = pde_t.val;
+    //temp= (uint32_t) pde_t;
+    paddr_write(tpaddr0,4,temp);
+    tpbase1 = pde_t.page_frame;
     tpbase1 = tpbase1<<12;
     tpaddr1 = tpbase1 + (tp1<<2);
-    PTE* pte_t;
-    pte_t = (PTE*)tpaddr1;
-    if(pte_t[0].present!=1)
+    PTE pte_t;
+    pte_t = (PTE)paddr_read(tpaddr1,4);;
+    if(pte_t.present!=1)
     {
         assert(0);
     }
-    printf("pte_t = %lx \n",(uintptr_t)pte_t);
-    pte_t[0].accessed = 1;
+    pte_t.accessed = 1;
     if(w==1)
     {
-        pte_t[0].dirty = 1;
+        pte_t.dirty = 1;
     }
+    temp= pte_t.val;
+    paddr_write(tpaddr1,4,temp);
     uint32_t pa_tt;
-    pa_tt =  pte_t->page_frame;
+    pa_tt =  pte_t.page_frame;
     pa_tt = pa_tt <<12;
     pa_tt = pa_tt + (addr_t&0xfff);
     return (paddr_t)pa_tt;
